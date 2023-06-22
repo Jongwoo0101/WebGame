12162aca212dde49f728129b6a58b38a970d6bfa
원종우 <96978536+Jongwoo0101@users.noreply.github.com>
2023-06-22 23:13:24 +0900

    first commit

---
+++ catch_game/script.js
@@ -0,0 +1,27 @@
+var score = 0;
+var gameContainer = document.getElementById("gameContainer");
+var mole = document.getElementById("mole");
+var scoreDisplay = document.getElementById("score");
+
+gameContainer.addEventListener("click", function() {
+    score++;
+    scoreDisplay.textContent = score;
+});
+
+function moveMole() {
+    var maxX = gameContainer.clientWidth - mole.clientWidth;
+    var maxY = gameContainer.clientHeight - mole.clientHeight;
+    var randomX = Math.floor(Math.random() * maxX);
+    var randomY = Math.floor(Math.random() * maxY);
+    
+    mole.style.left = randomX + "px";
+    mole.style.top = randomY + "px";
+    mole.style.display = "block";
+    
+    setTimeout(function() {
+        mole.style.display = "none";
+        setTimeout(moveMole, 1000);
+    }, 1000);
+}
+
+moveMole();
